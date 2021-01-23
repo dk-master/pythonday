b12dd8369291d5d892a26a460b5275b7efb7a925
dk-master <sunilair@naver.com>
2021-01-23 14:05:03 +0900

    [lv1] : 가운데글자

---
+++ algo/같은숫자는싫어.py
@@ -0,0 +1,7 @@
+def solution(arr):
+    answer = []
+    answer.append(arr[0])
+    for i in range (len(arr)-1) :
+        if(arr[i] != arr[i+1]):
+            answer.append(arr[i+1])
+    return answer

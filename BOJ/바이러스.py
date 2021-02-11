9f2cd07434e283b8ac9697e7e4911423874cbca7
dk-master <sunilair@naver.com>
2021-02-11 14:45:39 +0900

    바이러스 dfs

--- BOJ/바이러스.py
+++ BOJ/바이러스.py
@@ -1,15 +1,22 @@
 from collections import deque
 
-def BFS(graph,start,visited) :
-    queue = deque([start]) # start가 들어있는 리스트를 queue로 만들어준다.
-    visited[start] = True
+# def BFS(graph,start,visited) :
+#     queue = deque([start]) # start가 들어있는 리스트를 queue로 만들어준다.
+#     visited[start] = True
     
-    while queue :
-        v = queue.popleft()
-        for i in graph[v] :
-            if not visited[i] :
-                queue.append(i)
-                visited[i] = True
+#     while queue :
+#         v = queue.popleft()
+#         for i in graph[v] :
+#             if not visited[i] :
+#                 queue.append(i)
+#                 visited[i] = True
+
+def DFS(graph,start,visited) :
+    visited[start] = True
+
+    for i in graph[start] :
+        if not visited[i] :
+            DFS(graph,i,visited)
 
 n = int(input())
 m = int(input())
@@ -22,6 +29,7 @@ for i in range (m) :
 
     
 visited = [False] * (n+1)
-BFS(graph,1,visited)
+# BFS(graph,1,visited)
+DFS(graph,1,visited)
 print(visited)
 print(visited.count(True) - 1)

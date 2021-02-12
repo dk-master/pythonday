ab6813a85bb97a2f9c8c04abcf8757e17b7e1872
dk-master <sunilair@naver.com>
2021-02-12 23:36:57 +0900

    db practice

---
+++ dp/fibo.py
@@ -0,0 +1,18 @@
+d= [0] * 100
+
+def fibo(x) :
+    if x == 1 or x == 2:
+        return 1
+    if d[x] != 0:
+        return d[x]
+    d[x] = fibo(x-1) + fibo(x-2)
+    return d[x]
+print(fibo(99))
+# topDown
+
+d[1] = 1
+d[2] = 1
+n = 99
+
+for i in range(3,n+1) :
+    d[i] = d[i-1] + d[i-2]

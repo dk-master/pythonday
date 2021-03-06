93107c13c3d63cf03c85a4cd0a66e4063aa78c44
김동관 <dongkwan@gimdong-gwan-ui-MacBookAir.local>
2021-03-06 18:34:00 +0900

    greedy 시간초과 sys.stdin.readline()

---
+++ BOJ/신입사원.py
@@ -0,0 +1,26 @@
+import sys
+t = int(sys.stdin.readline())
+i =0
+result = []
+while True:
+    if i == t :
+        break
+    n = int(sys.stdin.readline())
+    answer = 1
+    graph = []
+    for _ in range(n):
+        graph.append(list(map(int,sys.stdin.readline().split())))
+    graph1 = sorted(graph,key = lambda x : x[0])
+    min = graph1[0][1]
+    for j in range(1,n):
+        if graph1[j][1] < min :
+            answer +=1
+            min = graph1[j][1]
+
+    i += 1
+    result.append(answer)
+
+
+
+for i in result :
+    print(i)
